4d030b4f1b147f1cbf773800cff6737021939f62
Jeff Kwok <jkwok97@gmail.com>
2019-05-12 09:23:15 -0600

    trying to get server running

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const cors = require('cors');
 const champions = require('./controllers/champions');
 const drafts = require('./controllers/drafts');
 const morgan = require('morgan');
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: process.env.POSTGRES_URI
+// });
 const knex = require('knex')({
     client: 'pg',
-    connection: process.env.POSTGRES_URI
-});
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'cha-hockey-app-server'
+    }
+})
 
 // console.log(knex.select('*').from('champions')
 // .then(data => {
@@ -21,13 +30,13 @@ app.use(cors());
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-    res.send(knex.owners);
+    res.send("it is working", knex.owners);
 });
 
 app.get('/champions/', (req, res) => {champions.handleChampionsGet(req, res, knex)});
 
 app.get('/drafts/', (req, res) => {drafts.handleDraftsGet(req, res, knex)});
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });

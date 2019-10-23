28db62eab18420a29c19ebd6137bef96f88cea4d
Jeff Kwok <jkwok97@gmail.com>
2019-10-23 11:45:32 -0600

    trying to get server running on heroku

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/champions/', (req, res) => {champions.handleChampionsGet(req, res, kne
 
 app.get('/drafts/', (req, res) => {drafts.handleDraftsGet(req, res, knex)});
 
-app.get('/players-stats/', (req, res) => {playersStats.handlePlayersStatsGet(req, res, knex)});
+app.get('/players-stats/', (req, res) => {player_stats.handlePlayersStatsGet(req, res, knex)});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);

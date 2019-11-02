2da4e71b46fc88f32fb00b9b0b8586956615902d
Jeff Kwok <jkwok97@gmail.com>
2019-11-02 10:06:21 -0600

    test

--- controllers/players_stats.js
+++ controllers/players_stats.js
@@ -3,24 +3,17 @@ const sportsDataURL = 'https://api.sportsdata.io/v3/nhl/scores/json/Players';
 const key = '4064a3b66cc64f28a8d52cc3be024ffb';
 
 const getAllPlayerInfo = (req, res) => {
-    // request(`${sportsDataURL}key=${key}`, function (error, response, body) {
-    //     if (!error && response.statusCode == 200) {
-    //         console.log(body);
-    //          var info = JSON.parse(body)
-    //         // do more stuff
-    //         res.send(info);
-    //     } else {
-    //         error => res.send(error)
-    //     }
-    //   })
-    request({url: `${sportsDataURL}key=${key}`, json: true}, (err, res, json) => {
-        if (err) {
-            res.status(400).json('error getting stats')
-            throw err;
+    request(`${sportsDataURL}key=${key}`, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+             var info = JSON.parse(body)
+            // do more stuff
+            res.send(info);
+        } else {
+            console.log(error);
+            error => res.send(error)
         }
-        console.log(json);
-        // res.json(data);
-    });
+      })
 }
 
 const handlePlayersStatsGet = (req, res, knex) => {

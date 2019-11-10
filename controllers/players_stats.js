f70fd5bed364e2979bda247874f4d6497e8b0854
Jeff Kwok <jkwok97@gmail.com>
2019-11-10 13:58:58 -0700

    trying to fix query

--- controllers/players_stats.js
+++ controllers/players_stats.js
@@ -63,6 +63,7 @@ const alltimePlayerStatsByType = (req, res, knex) => {
 }
 
 const alltimePlayerStatsGrouped = (req, res, knex) => {
+    console.log(req.query.type);
     knex.raw(`select season_type as season_type, sum(games_played) as games_played, sum(goals) as goals, sum(assists) as assists, sum(points) as points, sum(plus_minus) as plus_minus, sum(penalty_minutes) as penalty_minutes, sum(sh_goals) as sh_goals, sum(pp_goals) as pp_goals, sum(gw_goals) as gw_goals, sum(gt_goals) as gt_goals, sum(shots) as shots, sum(minutes_played) as minutes_played, sum(hits) as hits, sum(blocked_shots) as blocked_shots, player_name from players_stats where season_type = '${req.query.type}' group by player_name, season_type;`)
         .then(data => {
             res.json(data);

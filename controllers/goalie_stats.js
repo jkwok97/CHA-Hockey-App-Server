45c0276e6f329315b7ff5139a3fcf9c5b1290cc3
Jeff Kwok <jkwok97@gmail.com>
2019-11-10 16:12:22 -0700

    debugging goalie grouped stats

--- controllers/goalie_stats.js
+++ controllers/goalie_stats.js
@@ -105,7 +105,7 @@ const alltimeGoalieStatsGrouped = (req, res, knex) => {
 const allTimeTeamGoaliesStatsGrouped = (req, res, knex) => {
     console.log(req.params.teamName);
     console.log(req.query.type);
-    knex.raw(`select team_name as team_name, season_type as season_type, sum(games_played) as games_played, sum(wins) as wins, sum(loss) as loss, sum(ties) as ties, sum(minutes_played) as minutes_played, sum(en_goals) as en_goals, sum(shutouts) as shutouts, sum(goals_against) as goals_against, sum(saves) as saves, sum(shots_for) as shots_for, sum(goals) as goals, sum(assists) as assists, sum(points) as points, sum(penalty_minutes) as penalty_minutes, player_name from goalies_stats where team_name = '${req.params.teamName}' AND season_type = '${req.query.type}' group by player_name, team_name, season_type;`)
+    knex.raw(`select season_type as season_type, team_name as team_name, sum(games_played) as games_played, sum(wins) as wins, sum(loss) as loss, sum(ties) as ties, sum(minutes_played) as minutes_played, sum(en_goals) as en_goals, sum(shutouts) as shutouts, sum(goals_against) as goals_against, sum(saves) as saves, sum(shots_for) as shots_for, sum(goals) as goals, sum(assists) as assists, sum(points) as points, sum(penalty_minutes) as penalty_minutes, player_name from goalies_stats where team_name = '${req.params.teamName}' and season_type = '${req.query.type}' group by player_name, team_name, season_type;`)
         .then(data => {
             res.json(data);
         })

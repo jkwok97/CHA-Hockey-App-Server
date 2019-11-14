af529c08b5713d0d799a6101d1df3335f3ff33af
Jeff Kwok <jkwok97@gmail.com>
2019-11-14 11:40:11 -0700

    trying a different join

--- controllers/players.js
+++ controllers/players.js
@@ -1,6 +1,6 @@
 const getPlayerStats = (req, res, knex) => {
     console.log(req.params.name);
-    knex.select('*').from('players_stats as a').fullOuterJoin('nhlPlayers as b', 'a.player_name', '=', 'b.player_name')
+    knex.select('*').from('players_stats as a').join('nhl_players as b', 'a.player_name', '=', 'b.player_name')
         .where('a.player_name', req.params.name).orderBy('a.playing_year', 'desc')
         .then(data => {
             if (data.length) {
@@ -17,7 +17,7 @@ const getPlayerStats = (req, res, knex) => {
 
 const getPlayerStatsByType = (req, res, knex) => {
     console.log(req.params.name);
-    knex.select('*').from('players_stats as a').fullOuterJoin('nhl_players as b', 'a.player_name', '=', 'b.player_name')
+    knex.select('*').from('players_stats as a').join('nhl_players as b', 'a.player_name', '=', 'b.player_name')
         .where('a.player_name', req.params.name).where('a.season_type', req.query.type).orderBy('a.playing_year', 'desc')
         .then(data => {
             if (data.length) {

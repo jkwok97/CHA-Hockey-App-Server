0f848506286d1957ca6c7af10d060bb052595bd2
Jeff Kwok <jkwok97@gmail.com>
2019-11-14 11:23:03 -0700

    debugging

--- controllers/players.js
+++ controllers/players.js
@@ -19,19 +19,22 @@ const getPlayerStats = (req, res, knex) => {
 }
 
 const getPlayerStatsByType = (req, res, knex) => {
-    console.log(req.params.name);
-    knex.select('*').from('players_stats').fullOuterJoin('nhlPlayers', 'players_stats.player_name', '=', 'nhlPlayers.player_name')
-        .where('player_name', req.params.name).where('season_type', req.query.type).orderBy('playing_year', 'desc')
-        .then(data => {
-            if (data.length) {
-                res.json(data);
-            } else {
-                res.status(400).json('error getting stats')
-            }
-    })
-    .catch(err => {
-        console.log(err); 
-        res.status(400).json('not found');
+    // console.log(req.params.name);
+    // knex.select('*').from('players_stats').fullOuterJoin('nhl_players', 'players_stats.player_name', '=', 'nhl_players.player_name')
+    //     .where('player_name', req.params.name).where('season_type', req.query.type).orderBy('playing_year', 'desc')
+    //     .then(data => {
+    //         if (data.length) {
+    //             res.json(data);
+    //         } else {
+    //             res.status(400).json('error getting stats')
+    //         }
+    // })
+    // .catch(err => {
+    //     console.log(err); 
+    //     res.status(400).json('not found');
+    // })
+    knex.select('*').from('nhl_players').then(data => {
+        res.json(data);
     })
 }
 

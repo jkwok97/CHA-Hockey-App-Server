e26f824f2408ae85e4e62fdfcc9f2fe00c9aaba5
Jeff Kwok <jkwok97@gmail.com>
2019-11-14 11:14:08 -0700

    debugging

--- controllers/players.js
+++ controllers/players.js
@@ -1,17 +1,20 @@
 const getPlayerStats = (req, res, knex) => {
     console.log(req.params.name);
-    knex.select('*').from('players_stats').fullOuterJoin('nhlPlayers', 'players_stats.player_name', '=', 'nhlPlayers.player_name')
-        .where('player_name', req.params.name).orderBy('playing_year', 'desc')
-        .then(data => {
-            if (data.length) {
-                res.json(data);
-            } else {
-                res.status(400).json('error getting stats')
-            }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json('not found');
+    // knex.select('*').from('players_stats').fullOuterJoin('nhlPlayers', 'players_stats.player_name', '=', 'nhlPlayers.player_name')
+    //     .where('player_name', req.params.name).orderBy('playing_year', 'desc')
+    //     .then(data => {
+    //         if (data.length) {
+    //             res.json(data);
+    //         } else {
+    //             res.status(400).json('error getting stats')
+    //         }
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(400).json('not found');
+    // })
+    knex.select('*').from('nhlPlayers').then(data => {
+        res.json(data);
     })
 }
 

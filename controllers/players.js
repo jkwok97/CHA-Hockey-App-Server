462b2a76d3d90b73609a6f3e377fa555877e8991
Jeff Kwok <jkwok97@gmail.com>
2019-11-14 11:58:46 -0700

    taking out full outer join

--- controllers/players.js
+++ controllers/players.js
@@ -1,7 +1,8 @@
 const getPlayerStats = (req, res, knex) => {
     console.log(req.params.name);
 
-    knex.select('*').from('players_stats as a').fullOuterJoin('nhl_players as b', 'a.player_name', '=', 'b.player_name')
+    knex.select('*').from('players_stats as a')
+        // .fullOuterJoin('nhl_players as b', 'a.player_name', '=', 'b.player_name')
         .where('a.player_name', req.params.name).orderBy('a.playing_year', 'desc')
         .then(data => {
             if (data.length) {
@@ -18,7 +19,8 @@ const getPlayerStats = (req, res, knex) => {
 
 const getPlayerStatsByType = (req, res, knex) => {
     console.log(req.params.name);
-    knex.select('*').from('players_stats as a').fullOuterJoin('nhl_players as b', 'a.player_name', '=', 'b.player_name')
+    knex.select('*').from('players_stats as a')
+        // .fullOuterJoin('nhl_players as b', 'a.player_name', '=', 'b.player_name')
         .where('a.player_name', req.params.name).where('a.season_type', req.query.type).orderBy('a.playing_year', 'desc')
         .then(data => {
             if (data.length) {

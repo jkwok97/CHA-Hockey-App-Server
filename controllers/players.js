1467eb719eb596ab20c19a6a1aa19fd6cdac0f85
Jeff Kwok <jkwok97@gmail.com>
2019-12-11 15:32:38 -0700

    trying to figure answer

--- controllers/players.js
+++ controllers/players.js
@@ -129,7 +129,7 @@ getStats = (id) => {
     request(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=onPaceRegularSeason&season=20192020`, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             let info = JSON.parse(body);
-            return info;
+            // return info;
         } else {
             error => {
                 console.log(error);

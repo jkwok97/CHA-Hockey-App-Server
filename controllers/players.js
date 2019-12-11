016f5b27627907336607d42eaa9eefd8afc361a7
Jeff Kwok <jkwok97@gmail.com>
2019-12-11 10:51:22 -0700

    debugging'

--- controllers/players.js
+++ controllers/players.js
@@ -110,21 +110,21 @@ const getAllNHLPlayerStats = (req, res, knex) => {
     knex.select('*').from('nhl_players')
         .then( data => {
             if (data.length) {
-                data[0].forEach(player => {
+                data.forEach(player => {
                     console.log(player);
-                    request(`https://statsapi.web.nhl.com/api/v1/people/${player.player_nhl_id}/stats?stats=onPaceRegularSeason&season=20192020`, (error, response, body) => {
-                        if (!error && response.statusCode == 200) {
-                            var info = JSON.parse(body);
-                            player.stats = info;
-                            console.log(player);
-                        } else {
-                            error => {
-                                console.log(error);
-                                res.send(error);
-                            }
-                        }
-                    });
-                console.log(data);
+                    // request(`https://statsapi.web.nhl.com/api/v1/people/${player.player_nhl_id}/stats?stats=onPaceRegularSeason&season=20192020`, (error, response, body) => {
+                    //     if (!error && response.statusCode == 200) {
+                    //         var info = JSON.parse(body);
+                    //         player.stats = info;
+                    //         console.log(player);
+                    //     } else {
+                    //         error => {
+                    //             console.log(error);
+                    //             res.send(error);
+                    //         }
+                    //     }
+                    // });
+                // console.log(data);
                 });
                 res.json(data);
             } else {

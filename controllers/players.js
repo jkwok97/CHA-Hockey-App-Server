37095620e907bf26607c4690a01ceb4640596cc7
Jeff Kwok <jkwok97@gmail.com>
2019-12-11 13:45:47 -0700

    trying again

--- controllers/players.js
+++ controllers/players.js
@@ -110,12 +110,14 @@ const getAllNHLPlayerStats = (req, res, knex) => {
     knex.select('*').from('nhl_players')
         .then( data => {
             if (data.length) {
-                let playerArray = [];
-                data.forEach(player => {
+                let playerArray = data;
+                playerArray.forEach(player => {
                     request(`https://statsapi.web.nhl.com/api/v1/people/${player.player_nhl_id}/stats?stats=onPaceRegularSeason&season=20192020`, (error, response, body) => {
                         if (!error && response.statusCode == 200) {
                             var info = JSON.parse(body);
                             player.stats = info.stats[0]['splits'][0];
+                            console.log(player);
+                            playerArray.push(player);
                         } else {
                             error => {
                                 console.log(error);
@@ -123,13 +125,8 @@ const getAllNHLPlayerStats = (req, res, knex) => {
                             }
                         }
                     });
-                    // console.log(player);
-                    
-                    playerArray.push(player);
-
-                    
                 })
-                console.log(playerArray)
+                // console.log(playerArray)
                 res.json(playerArray);
             } else {
                 res.status(400).json('error getting stats');

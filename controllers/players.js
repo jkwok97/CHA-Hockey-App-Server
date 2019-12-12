95b5935129d0435f1d25fc18b8e086f95a6e2c7d
Jeff Kwok <jkwok97@gmail.com>
2019-12-12 13:46:41 -0700

    checking logs

--- controllers/players.js
+++ controllers/players.js
@@ -130,9 +130,11 @@ const getAllNHLPlayerStats = (req, res) => {
 }
 
 const getChaTeam = (req, res, knex) => {
+    console.log(req.query.player);
     knex.select('team_name').from('players_stats').where('player_name', req.query.player)
         .then(data => {
             if (data.length) {
+                console.log(data);
                 res.json(data);
             } else {
                 res.status(400).json('error getting player');

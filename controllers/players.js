5b6b069e48b97e1e9df20b27c955fe1013e47875
Jeff Kwok <jkwok97@gmail.com>
2019-12-12 11:29:17 -0700

    testing it out

--- controllers/players.js
+++ controllers/players.js
@@ -124,10 +124,11 @@ const getAllNHLPlayerStats = (req, res, knex) => {
     request(`${nhlCOM}/${req.query.playerType}s?reportType=${req.query.season}&reportName=${req.query.playerType}summary&cayenneExp=seasonId=${req.query.season}%20and%20gameTypeId=2&sort=${req.query.statType}`,
         (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                let leaders = body['data'];
-                console.log(leaders);
-                // let info = JSON.parse(leaders.reverse().splice(0,10));
-                res.send(leaders);
+                let leaders = JSON.parse(body);
+                let leadersArray = leaders.data;
+                console.log(leadersArray);
+                let info = leadersArray.reverse().splice(0,10);
+                res.send(info);
             } else {
                 error => {
                     console.log(error);

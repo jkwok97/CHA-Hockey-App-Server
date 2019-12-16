2ec2427087f69524c7c27632dd7898dbe9bb7597
Jeff Kwok <jkwok97@gmail.com>
2019-12-16 16:28:32 -0700

    changing the call

--- controllers/players.js
+++ controllers/players.js
@@ -167,7 +167,7 @@ const getForwardSalaries = (req, res, knex) => {
     knex.select('*').from('forward_salaries as a')
         .fullOuterJoin('players_stats as b', 'a.player_name', '=', 'b.player_name')
         .where('b.season_type', req.query.type).where('b.playing_year', req.query.year)
-        .orderBy('a.current_season_salary', 'desc')
+        // .orderBy('a.current_season_salary', 'desc')
         .then(data => {
             if (data.length) {
                 res.json(data);
@@ -185,7 +185,7 @@ const getDefenseSalaries = (req, res, knex) => {
     knex.select('*').from('defense_salaries as a')
         .fullOuterJoin('players_stats as b', 'a.player_name', '=', 'b.player_name')
         .where('b.season_type', req.query.type).where('b.playing_year', req.query.year)
-        .orderBy('a.current_season_salary', 'desc')
+        // .orderBy('a.current_season_salary', 'desc')
         .then(data => {
             if (data.length) {
                 res.json(data);
@@ -203,7 +203,7 @@ const getGoalieSalaries = (req, res, knex) => {
     knex.select('*').from('goalie_salaries as a')
         .fullOuterJoin('players_stats as b', 'a.player_name', '=', 'b.player_name')
         .where('b.season_type', req.query.type).where('b.playing_year', req.query.year)
-        .orderBy('a.current_season_salary', 'desc')
+        // .orderBy('a.current_season_salary', 'desc')
         .then(data => {
             if (data.length) {
                 res.json(data);

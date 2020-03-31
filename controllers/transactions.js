42c6a4b8b73d5f573eb333a280976c7780690c2b
Jeff Kwok <jkwok97@gmail.com>
2020-03-31 13:00:11 -0600

    test

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,10 +2,11 @@ const request = require('request');
 
 const acquire = (req, res, knex, waiversHookUrl) => {
 
-    console.log('players: ' + players);
-
     const players = req.body.players;
     const goalies = req.body.goalies;
+
+    console.log('players: ' + players);
+    console.log('goalies: ' + goalies);
 
     let error = false;
 
@@ -18,7 +19,8 @@ const acquire = (req, res, knex, waiversHookUrl) => {
                     } else {
                         error = true;
                     }
-                });
+                })
+                .catch(err => {res.status(400).json("Server Error!")});
         });
 
         console.log('players: ' + players);
@@ -34,8 +36,11 @@ const acquire = (req, res, knex, waiversHookUrl) => {
                     } else {
                         error = true;
                     }
-                });
-        })
+                })
+                .catch(err => {res.status(400).json("Server Error!")});
+        });
+
+        console.log('goalies: ' + goalies);
     }
 
     if (!error) {
@@ -73,29 +78,29 @@ module.exports = {
 
 
 // knex('draft_table').where({id: req.params.id}).update({round_five: req.body.team})
-    //     .then(resp => {
-    //         if (resp) {
-                // request.post(waiversHookUrl, {
-                //     json: {
-                //         'text': `:rotating_light: ${req.body.type} ALERT :rotating_light: \n \n ${req.body.prevTeam}'s round ${req.body.round} pick has been traded to ${req.body.team}`,
-                //         'channel': '#trades',
-                //         'username': 'League Office',
-                //         'icon_emoji': ':office:'
-                //     }
-                // }, (error, res, body) => {
-                //     if (error) {
-                //         console.log(error);
-                //         return
-                //     } else {
-                //         console.log(body);
-                //     }
-                // })
-        //         res.json("Success!")
-        //     } else {
-        //         res.status(400).json("Error Updating Player");
-        //     }
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        //     res.status(400).json("Server Error!")
-        // });
+//         .then(resp => {
+//             if (resp) {
+//                 request.post(waiversHookUrl, {
+//                     json: {
+//                         'text': `:rotating_light: ${req.body.type} ALERT :rotating_light: \n \n ${req.body.prevTeam}'s round ${req.body.round} pick has been traded to ${req.body.team}`,
+//                         'channel': '#trades',
+//                         'username': 'League Office',
+//                         'icon_emoji': ':office:'
+//                     }
+//                 }, (error, res, body) => {
+//                     if (error) {
+//                         console.log(error);
+//                         return
+//                     } else {
+//                         console.log(body);
+//                     }
+//                 })
+//                 res.json("Success!")
+//             } else {
+//                  res.status(400).json("Error Updating Player");
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(400).json("Server Error!")
+//         });

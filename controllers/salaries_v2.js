73fc51ea6d32f5aa2ceabd5ca222d72baf7e1ceb
Jeff Kwok <jkwok97@gmail.com>
2020-06-07 13:38:23 -0600

    add salaries controller

--- controllers/salaries_v2.js
+++ controllers/salaries_v2.js
@@ -19,8 +19,8 @@ const getAllSalaries = (req, res, knex) => {
 }
 
 const getAllActiveSalaries = (req, res, knex) => {
-    knex.select('*').from('players_v2')
-        .join('salaries_v2', {'players_v2.id': 'salaries_v2.players_id'})
+    knex.select('*').from('salaries_v2')
+        .fullOuterJoin('players_v2', {'salaries_v2.id': 'players_v2.id'})
         .where('players_v2.isactive', req.query.isactive)
         .then(data => {
             if (data.length) {

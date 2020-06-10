66ae2c8f39c81237e0125ba9cacc437194f4e7a3
Jeff Kwok <jkwok97@gmail.com>
2020-06-10 16:38:08 -0600

    debugging

--- controllers/draft_table.js
+++ controllers/draft_table.js
@@ -60,9 +60,12 @@ const getDraftPicksByTeam = (req, res, knex) => {
         )
         .from('draft_order_v2 as a')
         .leftJoin('teams_v2 as b', 'b.id', 'a.team_id')
-        .whereIn(req.param.id, ['a.team_id', 'a.round_one', 'a.round_two', 'a.round_three', 'a.round_four', 'a.round_five'])
-        .andWhere('a.draft_year', req.query.currentSeason)
-        .orWhere('a.draft_year', req.query.nextSeason)
+        .where(builder =>
+            builder.whereIn(['a.team_id', 'a.round_one', 'a.round_two', 'a.round_three', 'a.round_four', 'a.round_five'], [req.params.id]) 
+        )
+        .andWhere(builder => {
+            builder.where('a.draft_year', req.query.currentSeason).orWhere('a.draft_year', req.query.nextSeason)
+        })
         .then(data => {
             if (data.length) {
                 const result = {
@@ -77,65 +80,7 @@ const getDraftPicksByTeam = (req, res, knex) => {
         }).catch(err => {
             console.log(err)
             res.status(400).json('not found')}
-            )
-
-
-    // knex.select(
-    //     'a.*',
-    //     'b.shortname',
-    //     'b.city',
-    //     'b.nickname',
-    //     'b.teamlogo'
-    //     )
-    //     .from('draft_order_v2 as a')
-    //     .leftJoin('teams_v2 as b', 'b.id', 'a.team_id')
-    //     .whereIn(['a.team_id', 'a.round_one', 'a.round_two', 'a.round_three', 'a.round_four', 'a.round_five'], [req.params.id])
-    //     .andWhere('a.draft_year', req.query.currentSeason)
-    //     .orWhere('a.draft_year', req.query.nextSeason)
-    //     .then(data => {
-    //         if (data.length) {
-    //             const result = {
-    //                 statusCode: 200,
-    //                 message: 'Request Success',
-    //                 result: data[0]
-    //             }
-    //             res.json(result);
-    //         } else {
-    //             res.status(400).json('error getting pick stat')
-    //         }
-    //     }).catch(err => {
-    //         console.log(err)
-    //         res.status(400).json('not found')}
-    //         )
-
-
-    // knex.raw(
-    // `SELECT 
-    //     a.*,
-    //     b.shortname,
-    //     b.city,
-    //     b.nickname,
-    //     b.teamlogo
-    //     FROM draft_order_v2 as a 
-    //     LEFT JOIN teams_v2 as b 
-    //     ON a.team_id = b.id
-    //     WHERE ('${req.params.id}' in (team_id, round_one, round_two, round_three, round_four, round_five)
-    //     AND (a.draft_year = ${req.query.currentSeason} OR a.draft_year = ${req.query.nextSeason}));`
-    // ).then(data => {
-    //     if (data.length) {
-    //         const result = {
-    //             statusCode: 200,
-    //             message: 'Request Success',
-    //             result: data[0]
-    //         }
-    //         res.json(result);
-    //     } else {
-    //         res.status(400).json('error getting pick stat')
-    //     }
-    // }).catch(err => {
-    //     console.log(err);
-    //     res.status(400).json('not found')}
-    //     )
+        )
 }
 
 const updateDraftTableById = (req, res, knex) => {

aadeb0bdd845a090656ab45a96c5f0f6e766147f
Jeff Kwok <jkwok97@gmail.com>
2020-06-10 09:43:10 -0600

    added end point

--- controllers/goaliesStats_v2.js
+++ controllers/goaliesStats_v2.js
@@ -88,7 +88,9 @@ const getActiveGoaliesByTeam = (req, res, knex) => {
             } else {
                 res.status(400).json('error getting player stat')
             }
-        }).catch(err => res.status(400).json('not found'))
+        }).catch(err => {
+            console.log(err)
+            res.status(400).json('not found')})
 }
 
 const updateGoaliesStatsById = (req, res, knex) => {

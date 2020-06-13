650ecd2d374bdfe602a2d8176e1b50ba465e2384
Jeff Kwok <jkwok97@gmail.com>
2020-06-12 18:10:07 -0600

    fix

--- controllers/teams_v2.js
+++ controllers/teams_v2.js
@@ -98,7 +98,7 @@ const getUserIdByTeamName = (req, res, knex) => {
                 const result = {
                     statusCode: 200,
                     message: 'Request Success',
-                    result: data
+                    result: data[0]
                 }
                 res.json(result);
             } else {

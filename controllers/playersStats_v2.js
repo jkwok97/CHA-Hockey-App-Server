3cc8d256e072c7506f118713e57235ca1f876342
Jeff Kwok <jkwok97@gmail.com>
2020-06-15 16:01:39 -0600

    simplifying query

--- controllers/playersStats_v2.js
+++ controllers/playersStats_v2.js
@@ -154,12 +154,7 @@ const getPlayersByTypeByUser = (req, res, knex) => {
 
 getPlayersByShowByTypeByUser = (req, res, knex) => {
 
-    knex.select(
-        'a.player_id',
-        'a.season_type',
-        'a.team_name',
-        )
-        .from('players_stats_v2 as a')
+    knex('players_stats_v2 as a')
         .sum(
             'a.games_played as games_played',
             'a.goals as goals',
@@ -176,7 +171,7 @@ getPlayersByShowByTypeByUser = (req, res, knex) => {
             'a.hits as hits',
             'a.blocked_shots as blocked_shots'
         )
-        .groupByRaw('a.player_id, a.team_name, a.season_type, a.points')
+        .groupByRaw('a.player_id, a.team_name, a.season_type')
         .orderBy('a.points', 'desc')
         .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
         .leftJoin('teams_v2 as c', 'c.shortname', 'a.team_name')
@@ -197,6 +192,50 @@ getPlayersByShowByTypeByUser = (req, res, knex) => {
             console.log(err);
             res.status(400).json('not found')}
             )
+
+    // knex.select(
+    //     'a.player_id',
+    //     'a.season_type',
+    //     'a.team_name',
+    //     )
+    //     .from('players_stats_v2 as a')
+    //     .sum(
+    //         'a.games_played as games_played',
+    //         'a.goals as goals',
+    //         'a.assists as assists',
+    //         'a.points as points',
+    //         'a.plus_minus as plus_minus',
+    //         'a.penalty_minutes as penalty_minutes',
+    //         'a.sh_goals as sh_goals',
+    //         'a.pp_goals as pp_goals',
+    //         'a.gw_goals as gw_goals',
+    //         'a.gt_goals as gt_goals',
+    //         'a.shots as shots',
+    //         'a.minutes_played as minutes_played',
+    //         'a.hits as hits',
+    //         'a.blocked_shots as blocked_shots'
+    //     )
+    //     .groupByRaw('a.player_id, a.team_name, a.season_type, a.points')
+    //     .orderBy('a.points', 'desc')
+    //     .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
+    //     .leftJoin('teams_v2 as c', 'c.shortname', 'a.team_name')
+    //     .where('c.users_id', req.params.id)
+    //     .where('a.season_type', req.query.season_type)
+    //     .then(data => {
+    //         if (data.length) {
+    //             const result = {
+    //                 statusCode: 200,
+    //                 message: 'Request Success',
+    //                 result: data
+    //             }
+    //             res.json(result);
+    //         } else {
+    //             res.status(400).json('error getting player stat')
+    //         }
+    //     }).catch(err => {
+    //         console.log(err);
+    //         res.status(400).json('not found')}
+    //         )
 }
 
 const updatePlayersStatsById = (req, res, knex) => {

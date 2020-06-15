204dcb70675343bf65eb99da8a9830e3f80e14e8
Jeff Kwok <jkwok97@gmail.com>
2020-06-15 15:28:37 -0600

    debug

--- controllers/playersStats_v2.js
+++ controllers/playersStats_v2.js
@@ -155,12 +155,12 @@ const getPlayersByTypeByUser = (req, res, knex) => {
 getPlayersByShowByTypeByUser = (req, res, knex) => {
 
     knex.select(
-        'players_v2.firstname',
-        'players_v2.lastname',
-        'players_v2.isgoalie',
-        'teams_v2.city',
-        'teams_v2.nickname',
-        'teams_v2.teamlogo',
+        'b.firstname',
+        'b.lastname',
+        'b.isgoalie',
+        'c.city',
+        'c.nickname',
+        'c.teamlogo',
         'a.id',
         'a.season_type',
         'a.team_name',
@@ -182,7 +182,7 @@ getPlayersByShowByTypeByUser = (req, res, knex) => {
             'a.hits as hits',
             'a.blocked_shots as blocked_shots'
         )
-        .groupByRaw('a.player_id, a.team_name, a.season_type')
+        .groupByRaw('a.player_id, a.team_name, a.season_type, b.firstname, b.lastname, b.isgoalie, c.city, c.nickname, c.teamlogo')
         .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
         .leftJoin('teams_v2 as c', 'c.shortname', 'a.team_name')
         .where('c.users_id', req.params.id)

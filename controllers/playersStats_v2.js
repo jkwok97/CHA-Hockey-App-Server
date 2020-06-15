d700f41cb4b78353c40cdbc1034b39e968fe2717
Jeff Kwok <jkwok97@gmail.com>
2020-06-15 16:55:54 -0600

    trying new endpoint

--- controllers/playersStats_v2.js
+++ controllers/playersStats_v2.js
@@ -154,87 +154,57 @@ const getPlayersByTypeByUser = (req, res, knex) => {
 
 getPlayersByShowByTypeByUser = (req, res, knex) => {
 
-    knex('players_stats_v2 as a')
-        .sum([
-            'a.games_played as games_played',
-            'a.goals as goals',
-            'a.assists as assists',
-            'a.points as points',
-            'a.plus_minus as plus_minus',
-            'a.penalty_minutes as penalty_minutes',
-            'a.sh_goals as sh_goals',
-            'a.pp_goals as pp_goals',
-            'a.gw_goals as gw_goals',
-            'a.gt_goals as gt_goals',
-            'a.shots as shots',
-            'a.minutes_played as minutes_played',
-            'a.hits as hits',
-            'a.blocked_shots as blocked_shots'
-        ])
-        .groupByRaw('a.player_id, a.team_name, a.season_type')
-        .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
-        .leftJoin('teams_v2 as c', 'c.shortname', 'a.team_name')
-        .where('c.users_id', req.params.id)
-        .where('a.season_type', req.query.season_type)
-        .then(data => {
-            if (data.length) {
-                const result = {
-                    statusCode: 200,
-                    message: 'Request Success',
-                    result: data
-                }
-                res.json(result);
-            } else {
-                res.status(400).json('error getting player stat')
-            }
-        }).catch(err => {
-            console.log(err);
-            res.status(400).json('not found')}
-            )
 
-    // knex.select(
-    //     'a.player_id',
-    //     'a.season_type',
-    //     'a.team_name',
-    //     )
-    //     .from('players_stats_v2 as a')
-    //     .sum(
-    //         'a.games_played as games_played',
-    //         'a.goals as goals',
-    //         'a.assists as assists',
-    //         'a.points as points',
-    //         'a.plus_minus as plus_minus',
-    //         'a.penalty_minutes as penalty_minutes',
-    //         'a.sh_goals as sh_goals',
-    //         'a.pp_goals as pp_goals',
-    //         'a.gw_goals as gw_goals',
-    //         'a.gt_goals as gt_goals',
-    //         'a.shots as shots',
-    //         'a.minutes_played as minutes_played',
-    //         'a.hits as hits',
-    //         'a.blocked_shots as blocked_shots'
-    //     )
-    //     .groupByRaw('a.player_id, a.team_name, a.season_type, a.points')
-    //     .orderBy('a.points', 'desc')
-    //     .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
-    //     .leftJoin('teams_v2 as c', 'c.shortname', 'a.team_name')
-    //     .where('c.users_id', req.params.id)
-    //     .where('a.season_type', req.query.season_type)
-    //     .then(data => {
-    //         if (data.length) {
-    //             const result = {
-    //                 statusCode: 200,
-    //                 message: 'Request Success',
-    //                 result: data
-    //             }
-    //             res.json(result);
-    //         } else {
-    //             res.status(400).json('error getting player stat')
-    //         }
-    //     }).catch(err => {
-    //         console.log(err);
-    //         res.status(400).json('not found')}
-    //         )
+    knex.raw(`
+    select
+    b.firstname,
+    b.lastname,
+    b.isgoalie,
+    c.city,
+    c.nickname,
+    c.teamlogo,
+    a.season_type as season_type, 
+    a.team_name as team_name, 
+    sum(a.games_played) as games_played, 
+    sum(a.goals) as goals, 
+    sum(a.assists) as assists, 
+    sum(a.points) as points, 
+    sum(a.plus_minus) as plus_minus, 
+    sum(a.penalty_minutes) as penalty_minutes, 
+    sum(a.sh_goals) as sh_goals, 
+    sum(a.pp_goals) as pp_goals, 
+    sum(a.gw_goals) as gw_goals, 
+    sum(a.gt_goals) as gt_goals, 
+    sum(a.shots) as shots, 
+    sum(a.minutes_played) as minutes_played, 
+    sum(a.hits) as hits, 
+    sum(a.blocked_shots) as blocked_shots
+    from
+    players_stats_v2 as a,
+    players_v2 as b,
+    teams_v2 as c
+    where b.id = a.player_id
+    and
+    team_name = 'STA'
+    AND
+    season_type = 'Regular' 
+    group by b.firstname, b.lastname, b.isgoalie, a.season_type, a.team_name, c.city, c.nickname, c.teamlogo
+    ;`)
+    .then(data => {
+        if (data.length) {
+            const result = {
+                statusCode: 200,
+                message: 'Request Success',
+                result: data
+            }
+            res.json(result);
+        } else {
+            res.status(400).json('error getting player stat')
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json('not found')}
+        )
 }
 
 const updatePlayersStatsById = (req, res, knex) => {

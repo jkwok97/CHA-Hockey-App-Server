52f8a21b119d70a8d659b31a851131d6ce89c001
Jeff Kwok <jkwok97@gmail.com>
2020-06-19 09:35:10 -0600

    added all goalie stats for season

--- controllers/goaliesStats_v2.js
+++ controllers/goaliesStats_v2.js
@@ -94,6 +94,7 @@ const getActiveGoaliesByTeam = (req, res, knex) => {
 }
 
 const getGoaliesBySeasonByType = (req, res, knex) => {
+    console.log("hello");
     knex.select(
         'a.*',
         'b.firstname',

944114fb74803dfe20bab89d22744662b87f510b
Jeff Kwok <jkwok97@gmail.com>
2020-06-23 15:13:04 -0600

    test:

--- controllers/draft_table.js
+++ controllers/draft_table.js
@@ -29,15 +29,16 @@ const getDraftTableByYearByStandings = (req, res, knex) => {
         'b.shortname',
         'b.city',
         'b.nickname',
-        'b.teamlogo'
+        'b.teamlogo',
+        'c.points'
         )
         .from('draft_order_v2 as a')
         .leftJoin('teams_v2 as b', 'b.id', 'a.team_id')
-        .leftJoin('team_stats_v2 as c', 'c.team_id', 'a.team_id')
+        .leftJoin('team_stats_v2 as c', 'c.team_id', 'b.id')
         .where('a.draft_year', req.query.draft_year)
-        .where('c.playing_year', req.query.playing_year)
-        .where('c.season_type', req.query.season_type)
-        .orderBy('c.points', 'desc')
+        // .where('c.playing_year', req.query.playing_year)
+        // .where('c.season_type', req.query.season_type)
+        // .orderBy('c.points', 'asc')
         .then(data => {
             if (data.length) {
                 const result = {

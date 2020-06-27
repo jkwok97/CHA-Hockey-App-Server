cddd2e719b28a826e8d91b69c8a7de94bc75ded4
Jeff Kwok <jkwok97@gmail.com>
2020-06-26 18:30:11 -0600

    debug

--- controllers/ratings_v2.js
+++ controllers/ratings_v2.js
@@ -4,10 +4,11 @@ const getPlayerRatings = (req, res, knex) => {
         'b.firstname',
         'b.lastname',
         'b.isgoalie',
-        'b.nhl_id',
+        'b.nhl_id'
         )
         .from('player_ratings_v2 as a')
         .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
+        .where('a.player_id', req.params.id)
         .where('a.playing_year', req.query.playing_year)
         .then(data => {
             if (data.length) {
@@ -29,10 +30,11 @@ const getGoalieRatings = (req, res, knex) => {
         'b.firstname',
         'b.lastname',
         'b.isgoalie',
-        'b.nhl_id',
+        'b.nhl_id'
         )
         .from('goalie_ratings_v2 as a')
         .leftJoin('players_v2 as b', 'b.id', 'a.player_id')
+        .where('a.player_id', req.params.id)
         .where('a.playing_year', req.query.playing_year)
         .then(data => {
             if (data.length) {
